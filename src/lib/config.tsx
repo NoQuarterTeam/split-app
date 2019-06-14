9224b2c8116c84573ffc6c3c346cfe098a7f3938
Jack Clackett <jclackett@gmail.com>
2019-06-14 11:52:19 +0200

    update prod api url

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -4,6 +4,5 @@ export const production = env === "production"
 
 export const apiUrl =
   env === "production"
-    ? "https://nq-split.herokuapp.com/graphql"
-    : // : "http://192.168.43.77:5000/graphql"
-      "http://localhost:5000/graphql"
+    ? "https://api.getsplit.co/graphql"
+    : "http://localhost:5000/graphql"

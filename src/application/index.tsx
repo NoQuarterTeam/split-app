9e7099f154c8aecdbd7bc8bffa615db040d16cd5
Jack Clackett <jack@noquarter.co>
2019-08-13 20:54:10 +0200

    add dark status bar

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,4 +1,5 @@
-import React, { Suspense } from "react"
+import React, { Suspense, Fragment } from "react"
+import { StatusBar } from "react-native"
 import "../lib/prototypes"
 import AppProvider from "../components/providers/AppProvider"
 import CheckUser from "../components/CheckUser"
@@ -8,16 +9,19 @@ import CheckHouse from "../components/CheckHouse"
 
 function Application() {
   return (
-    <Suspense fallback={null}>
-      <AppProvider>
-        <CheckUser>
-          <CheckHouse>
-            <Routes />
-            <BottomBar />
-          </CheckHouse>
-        </CheckUser>
-      </AppProvider>
-    </Suspense>
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <Suspense fallback={null}>
+        <AppProvider>
+          <CheckUser>
+            <CheckHouse>
+              <Routes />
+              <BottomBar />
+            </CheckHouse>
+          </CheckUser>
+        </AppProvider>
+      </Suspense>
+    </Fragment>
   )
 }
 
